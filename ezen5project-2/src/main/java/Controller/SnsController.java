99130a0db9af02bb20f3c6fb997614999ac19bd9
504 <504@DESKTOP-SI28KUP>
2023-09-04 17:28:08 +0900

    병철 17시 28분 수정

--- ezen5project-2/src/main/java/Controller/SnsController.java
+++ ezen5project-2/src/main/java/Controller/SnsController.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import model.dao.SnsDao;
+import model.dto.SnsDto;
+
 
 @WebServlet("/SnsController")
 public class SnsController extends HttpServlet {
@@ -32,7 +38,36 @@ public class SnsController extends HttpServlet {
 	// 글 수정 - 병철
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	}
+		MultipartRequest multi = new MultipartRequest(
+				request, 
+				request.getServletContext().getRealPath("/sns/img"),
+				1024*1024*1024,
+				"UTF-8",
+				new DefaultFileRenamePolicy()
+				);
+		// 수정할 게시물 번호
+		int sno = Integer.parseInt(multi.getParameter("sno"));
+		
+		// 수정사항 객체화
+		String simg = multi.getFilesystemName("simg");
+		String scontent = multi.getParameter("scontent");
+		// 본인확인 비밀번호 객체화
+		String spwd = multi.getParameter("spwd");
+		// 수정할 게시물 번호와 비밀번호가 db 필드값과 같을경우 수정 진행
+		
+		SnsDto dto = new SnsDto(sno, simg , scontent);
+		if (dto.getSimg() == null) {
+	//		dto.setSimg(SnsDao.getInstance().getPrintContent(sno).getSimg;
+		}
+		
+		boolean result = SnsDao.getInstance().onupdate(dto);
+		
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().print(result);		
+		
+		
+		
+	}// doPut end
 
 	// 글 삭제 - 승우
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
