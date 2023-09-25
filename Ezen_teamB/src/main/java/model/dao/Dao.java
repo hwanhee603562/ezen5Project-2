3c1256ac920d19bd2e9301e9aae40d59e49b4753
504 <504@DESKTOP-BD9B2H9>
2023-09-25 16:26:46 +0900

    Merge branch '최의선' of https://github.com/hwanhee603562/ezen5Project-2

--- Ezen_teamB/src/main/java/model/dao/Dao.java
+++ Ezen_teamB/src/main/java/model/dao/Dao.java
@@ -26,7 +26,7 @@ public class Dao {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");	
 
-
+			
 
 			this.conn = DriverManager.getConnection("jdbc:mysql://database-1.ccmmlpcfyswe.us-east-1.rds.amazonaws.com:3306/usedtrade"
 							, "admin", "12341234a");
