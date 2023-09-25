56dc886805efb07098c33a15d3bc0bb8f74484d3
504 <504@DESKTOP-CM43SMF>
2023-09-25 17:30:40 +0900

    판매목록 수정, 삭제 버튼

--- Ezen_teamB/src/main/webapp/js/chatting/chatting.js
+++ Ezen_teamB/src/main/webapp/js/chatting/chatting.js
@@ -6,8 +6,13 @@ let clientSocket = new WebSocket(`ws://localhost:80/jspweb/serversocket/${loginM
 
 function onSend(){ console.log('전송함수');
 
+	let msgvalue = document.querySelector('.msg').value;
+	if(msgvalue == '' || msgvalue == '\n'){return;}	// 공백이나 엔터키 입력시 메시지 전송 x
 	
+	let msg = {type : 'message', content : msgvalue}
 	
+	clientSocket.send(JSON.stringify(msg));
+	document.querySelector('.msg').value = ``;
 	
 }
 
