defb6bf2941c75a9438493596831513c52b5095b
504 <504@DESKTOP-BD9B2H9>
2023-09-26 17:49:47 +0900

    Merge branch '최병철' of https://github.com/hwanhee603562/ezen5Project-2

--- Ezen_teamB/src/main/java/model/dao/Dao.java
+++ Ezen_teamB/src/main/java/model/dao/Dao.java
@@ -17,7 +17,7 @@ public class Dao {
 			/*
 			Class.forName("com.mysql.cj.jdbc.Driver");	
 			
-				
+					
 			
 			this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/usedtrade"
 					, "root", "1234");
